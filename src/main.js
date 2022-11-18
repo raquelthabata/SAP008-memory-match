131a9112b058360746ef06fc048b98d82508d4b2
raquelthabata <r.thabata05@gmail.com>
2022-11-18 18:09:13 -0300

    tela de login inicial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+import pokemon from './data/pokemon/pokemon.js'
+import data from './data/pokemon/pokemon.js'
+
+let pokemons = data.items
+console.log(pokemons)
+
+
+pokemons.forEach((pokemon)=>{
+    console.log(pokemon.id)
+})
+
+
+//tenho que entrar primeiro no array dp no objeto
+/*for(let pokemonName of pokemons){
+        console.log(pokemonName.id)
+    }*/
