7a1edb6198ac5a9dcb2275a96cd5efd743893ebb
raquelthabata <r.thabata05@gmail.com>
2022-11-21 23:41:55 -0300

    refazendo função de grid

--- src/game.js
+++ src/game.js
@@ -1,17 +1,6 @@
 
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js'
-
-const pokemons = data.items;
-let cardHtml = '';
-const gerarImg = (objeto)=>{
-    objeto.forEach((objeto)=>{
-        objeto.image 
-    })
-}
-
-gerarImg(pokemons)
-
-
 
 //1: a gente precisa criar toda a estrutura doq está no html para aparecer nossa carta, quem vai criar essa estrutura é função que irei criar
  /*
@@ -24,6 +13,7 @@ gerarImg(pokemons)
 */
 
 //1.1 selecionei onde será criado os cards que no caso é na div grid lá no html
+
 const grid = document.querySelector('.grid'); 
 
 //1.2 função parar criar o card
@@ -32,7 +22,7 @@ const grid = document.querySelector('.grid');
 //método .appendChild(elemento que vai receber) adiciona um filho a um elemento no HTML
 
 
-const createCard = () => {
+/*const createCard = () => {
     const card = document.createElement('div');
     const front = document.createElement('div');
     const back = document.createElement('div');
@@ -56,146 +46,67 @@ const createCard = () => {
 }
 
 createCard()
+//chamando a função para ser executada, ela não recebe nenhum parâmetro 
+*/
 
+//1.3 na função acima criei fiz muitas repetições de códigos , para facilitar, vou criar uma função para criar os elementos
 
-
-
-/*const createCard = (pokemonName) => {
-    const card = createElements('div', 'card');
-    const front = createElements('div', ' face front');
-    const back = createElements('div', ' face back');
-
-    front.style.backgroundImage = gerarImg(pokemons);
-
-    card.appendChild(front);
-    card.appendChild(back);
-
-    card.addEventListener('click', revealCard);
-    card.setAttribute('data-pokemonName', pokemonName);
-
-    return card;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const spanPlayer = document.querySelector ('.player'); // onde será o nome do jogador 
-
-const time = document.querySelector('.timer') // onde aparecerá o tempo de jogo*/
-
-
-
-
-
-
-/*const createElements = (tag, nameOfClass) => {
+const createElements = (tag, nameOfClass)=>{
     const element = document.createElement(tag);
     element.className = nameOfClass;
     return element;
 }
+ //essa função está recebendo como parametro o nome da tag e o nome da classe que queremos criar 
+ // criei uma constante chamada elemento que vai receber o método document.creatElements, o elemento que ela vai criar passamos através da tag que é o parametro colocado na função
+ //em seguida peguei o elemento e adicionei através do método className a classe que ele irá criar, e o nome dessa classe será o parametro que a função irá receber
+ //por fim é retornado o elemento pronto 
 
-const checkEndGame = () =>{
-const disabledCard = document.querySelectorAll('.disabled-card');
-if (disabledCard.length === 18){
-    clearInterval(this.loop);
-alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${time.innerHTML}`);
+ //1.4 agora reaproveitando a função de criar elemento para a função de criar o card, ela ficará assim:
+
+ const pokemons = data.items;
+
+
+const gerarImg = (objeto)=>{
+    const imgItems = [];
+    objeto.forEach((objeto)=>{
+        imgItems.push(objeto.image)
+
+    })
+
+    return imgItems;
 }
 
+const pokemonsImages = gerarImg(pokemons)
+
+const createCard = (pokemon) => {
+    const card = createElements('div', 'card');
+    const front = createElements('div', ' face front');
+    const back = createElements('div', ' face back');
+
+    front.style.backgroundImage = `url(${pokemon})`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
 
 }
-
-let firstCard = '';
-let secondCard = '';
-
-const checkCards = () => {
-    const firstPokemon = firstCard.getAttribute('data-pokemonName');
-    const secondPokemon = secondCard.getAttribute('data-pokemonName');
-
-    if (firstPokemon === secondPokemon) {
-        firstCard.firstChild.classList.add('disabled-card');
-        secondCard.firstChild.classList.add('disabled-card');
-        firstCard = '';
-        secondCard = '';
-
-        checkEndGame()
-    } else {
-        setTimeout(() => {
-            firstCard.classList.remove('reveal-card');
-            secondCard.classList.remove('reveal-card');
-            firstCard = '';
-            secondCard = '';
-
-        }, 600)
-
-    }
-}
+//na constante card, front e back que criei ao invés de utilizar o método createElements, chamo a função que criei que é para criar os elementos e passamos os dois parametros que ela recebe, o nome da tag, ex: div e o nome da classe, ex: card
+//depois é adicionado normalmente as tags front back como filhos da tag card 
+//por fim retornado o card
 
 
+//chamando a função
+//essa função é somente para criar a carta
 
-const revealCard = ({ target }) => {
-    if (target.parentNode.className.includes('reveal-card')) {
-        return;
-    }
-
-
-    if (firstCard === '') {
-        target.parentNode.classList.add('reveal-card');
-        firstCard = target.parentNode;
-    } else if (secondCard === '') {
-        target.parentNode.classList.add('reveal-card');
-        secondCard = target.parentNode;
-
-        checkCards()
-    }
-}
+//2: preciso criar uma função para carregar o jogo
 
 
 const loadGame = () => {
-    const duplicatePokemons = [...pokemons, ...pokemons];
-
-    const embaralharArray = duplicatePokemons.sort(() => Math.random() - 0.5);
-
-    embaralharArray.forEach((pokemonName) => {
-        const card = createCard(pokemonName)
-        grid.appendChild(card);
-    });
+ pokemonsImages.forEach((pokemon)=>{
+    const card = createCard(pokemon);
+    grid.appendChild(card);
+ });
 }
 
-const startTimer = ()=>{
-    this.loop = setInterval(()=>{
-        const currentTimer = Number(time.innerHTML);
-         time.innerHTML = currentTimer + 1;
-    }, 1000)
-}
-
-window.onload = () =>{
-    const playerName = localStorage.getItem('player');
-    spanPlayer.innerHTML = playerName;
-    startTimer()
-    loadGame()
-};
-*/
+loadGame()
